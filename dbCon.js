186ac3b46f86f7982c6e5ad33421c1ad113bac22
Afjal <mdafjalhussain564@gmail.com>
2026-08-28 18:53:19 +0530

    Fix Aiven MySQL SSL connection

--- dbCon.js
+++ dbCon.js
@@ -33,7 +33,7 @@ const db = mysql.createConnection({
     database: process.env.DB_NAME,
     port: process.env.DB_PORT || 3306,
     ssl: {
-        rejectUnauthorized: true
+        rejectUnauthorized: false
     }
 });
 
