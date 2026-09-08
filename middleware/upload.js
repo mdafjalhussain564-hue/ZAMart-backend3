9a5a5b260820f4fb64024f964917ddd4c8c911e4
Afjal <mdafjalhussain564@gmail.com>
2026-09-08 11:01:36 +0530

    upload js

--- middleware/upload.js
+++ middleware/upload.js
@@ -2,7 +2,8 @@ const multer = require("multer");
 const path = require("path");
 const fs = require("fs");
 
-const uploadDir = "uploads";
+// middleware folder se ek level bahar jaakar uploads
+const uploadDir = path.join(__dirname, "..", "uploads");
 
 if (!fs.existsSync(uploadDir)) {
     fs.mkdirSync(uploadDir, { recursive: true });
